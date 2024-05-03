789a855db0931826941309a16efc423514935544
Sidd1826 <18siddhum@gmail.com>
2024-05-03 14:48:35 +0530

    last commit

--- tests/test.py
+++ tests/test.py
@@ -11,8 +11,8 @@ from selenium.webdriver.chrome.options import Options as ChromeOptions
 # so this will be overridden and tests will run browserstack -
 # without any changes to the test files!
 options = ChromeOptions()
-# options.set_capability('sessionName', 'BStack Sample Test')
-driver = webdriver.Chrome()
+options.set_capability('sessionName', 'BStack Sample Test')
+driver = webdriver.Chrome(options=options)
 
 try:
     # Open flipkart.com
